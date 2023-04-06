b6701a6e24fe7851a005c701bd65a19aacb4c757
anonmy2021 <melaniefed03@gmail.com>
2023-04-06 11:31:39 +0300

    Initial commit

---
+++ main.rb
@@ -0,0 +1,27 @@
+class Main
+  def self.main
+    puts "Enter the text:"
+    text = gets.chomp.downcase
+    words = text.scan(/[a-zA-Z0-9]+/)
+
+    word_count = {}
+    words.each do |word|
+      if word_count[word]
+        word_count[word] += 1
+      else
+        word_count[word] = 1
+      end
+    end
+
+    sorted = word_count.sort_by { |word, count| -count }
+
+    puts "\nThe most often words to meet:"
+    sorted.each_with_index do |(word, count), index|
+      break if index >= 3
+      puts "#{word}: #{count}"
+    end
+  end
+
+  Main.main()
+
+end
